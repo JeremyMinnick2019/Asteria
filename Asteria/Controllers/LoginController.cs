492f10f04eaa8cc8ae4fe94e8c3f8a4165b3d53b
JeremyMinnick2019 <54860481+JeremyMinnick2019@users.noreply.github.com>
2022-07-12 18:37:14 -0400

    Login functionality (#33)
    * Added login functionality
    
    * code clean up
    
    * added more session options and check if logged in
    
    Co-authored-by: Jeremy <jeremyminnick2019@gmail.com>

---
+++ Asteria/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Asteria.Classes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asteria.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult LoginPage()
+        {
+            if (HttpContext.Session.TryGetValue("IsLoggIn", out var Hold))
+            {
+                return Redirect(Url.Action("index", "Home"));
+            }
+
+            return View();
+        }
+
+        public JsonResult LoginVerification(string APIKey)
+        {
+            string URL = "https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=" + APIKey;
+
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = client.GetAsync(URL).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                HttpContext.Session.Set<bool>("IsLoggIn", true);
+                HttpContext.Session.Set("APIKey", APIKey);
+                return Json(new { response = true });
+            }
+            else
+            {
+                return Json(new { response = false });
+            }
+        }
+    }
+}
