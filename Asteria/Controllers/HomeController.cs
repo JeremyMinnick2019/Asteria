964ba525d31ff0567078b680122c9a3ebad70060
JeremyMinnick2019 <54860481+JeremyMinnick2019@users.noreply.github.com>
2022-07-21 17:26:05 -0400

    Home page (#42)
    * Dev (#39)
    
    * Add or update the Azure App Service build and deployment workflow config
    
    * Login functionality (#33)
    
    * Added login functionality
    
    * code clean up
    
    * added more session options and check if logged in
    
    Co-authored-by: Jeremy <jeremyminnick2019@gmail.com>
    
    * Login functionality (#33) (#34)
    
    * Added login functionality
    
    * code clean up
    
    * added more session options and check if logged in
    
    Co-authored-by: Jeremy <jeremyminnick2019@gmail.com>
    
    Co-authored-by: Jeremy <jeremyminnick2019@gmail.com>
    
    * Update workflow (#35)
    
    * Add or update the Azure App Service build and deployment workflow config
    
    * Login functionality (#33) (#34)
    
    * Added login functionality
    
    * code clean up
    
    * added more session options and check if logged in
    
    Co-authored-by: Jeremy <jeremyminnick2019@gmail.com>
    
    Co-authored-by: Jeremy <jeremyminnick2019@gmail.com>
    
    * Updated to check dev and QA
    
    * Changed to just dev
    
    Co-authored-by: Jeremy <jeremyminnick2019@gmail.com>
    
    * Login functionality (#36)
    
    * Added login functionality
    
    * code clean up
    
    * added more session options and check if logged in
    
    * Added login check to HomeController
    
    Co-authored-by: Jeremy <jeremyminnick2019@gmail.com>
    
    * Delete qa_asteriaconnect-8311318f-2855-40a8-a4e7-b330dd502760-qa.yml
    
    Co-authored-by: Jeremy <jeremyminnick2019@gmail.com>
    
    * Delete qa_asteriaconnect-8311318f-2855-40a8-a4e7-b330dd502760-qa.yml
    
    * Added pic of the day
    
    * Support for vscode and added .DS_Store to .gitignore
    
    * Added constants, code gen, updated docs
    
    * Added card and info for photo
    
    * fixed style
    
    * Finished Home Page
    
    Co-authored-by: Jeremy <jeremyminnick2019@gmail.com>
    Co-authored-by: “Jeremy <“jeremyminnick2019@gmail.com”>

--- Asteria/Controllers/HomeController.cs
+++ Asteria/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Asteria.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Newtonsoft.Json.Linq;
 
 namespace Asteria.Controllers
 {
@@ -15,9 +16,51 @@ namespace Asteria.Controllers
         {
             if (!HttpContext.Session.TryGetValue("IsLoggIn", out var Hold) || !HttpContext.Session.Get<bool>("IsLoggIn"))
             {
-                return Redirect(Url.Action("LoginPage", "Login"));
+                return Redirect(Url.Action("LoginPage", "Login") ?? Constants.ErrorRoute);
             }
-            return View();
+
+            APOD PictureOfTheDayValue = GetPictureOfTheDay();
+
+            return View(PictureOfTheDayValue);
+        }
+
+        
+
+        public APOD GetPictureOfTheDay() 
+        {
+            string? APIKey = HttpContext.Session.Get<string>("APIKey");
+
+            string URL = $"https://api.nasa.gov/planetary/apod?api_key={APIKey}";
+
+            HttpClient Client = new HttpClient();
+
+            HttpResponseMessage Resp = Client.GetAsync(URL).Result;
+
+            string Obj = Resp.Content.ReadAsStringAsync().Result;
+
+            JObject PicOfDayObj = JObject.Parse(Obj);
+
+            APOD APODInfo = new APOD()
+            {
+                CopyRight = (string?)PicOfDayObj["copyright"],
+                Date = (string?)PicOfDayObj["date"] ?? "",
+                Explanation = (string?)PicOfDayObj["explanation"],
+                Hdurl = PhotoLinkToByteArray((string?)PicOfDayObj["hdurl"]),
+                MediaType = (string?)PicOfDayObj["media_type"],
+                Title = (string?)PicOfDayObj["title"],
+            };
+
+            return APODInfo;
+        }
+
+        public byte[]? PhotoLinkToByteArray(string? Link)
+        {
+            if(string.IsNullOrWhiteSpace(Link))
+                return null;
+
+            HttpClient Client = new HttpClient();
+
+            return Client.GetByteArrayAsync(Link).Result;
         }
     }
 }
---
+++ Asteria/Models/APOD.cs
@@ -0,0 +1,37 @@
+﻿namespace Asteria.Models
+{
+    public class APOD
+    {
+        public string? CopyRight { get; set; }
+
+        private string? date;
+
+        public string Date
+        {
+             get
+             {
+                if(this.date == null)
+                {
+                    return "No Date Available";
+                }
+
+                return this.date;
+             } 
+
+             set
+             {
+                if(value == null)
+                {
+                    this.date = "No Date Available";
+                    return;
+                }
+                    
+                this.date = DateTime.Parse(value).ToString("MM/dd/yyyy");
+             }
+        }
+        public string? Explanation { get; set; }
+        public byte[]? Hdurl { get; set; }
+        public string? MediaType { get; set; }
+        public string? Title { get; set; }
+    }
+}
